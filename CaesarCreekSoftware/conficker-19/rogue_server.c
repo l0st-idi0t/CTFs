ca225ab014518e86f20831581a338f98bc6f33c7
l0st-idi0t <carlnod121@gmail.com>
2023-09-25 13:14:43 -0500

    progress

--- CaesarCreekSoftware/conficker-19/rogue_server.c
+++ CaesarCreekSoftware/conficker-19/rogue_server.c
@@ -1,91 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <openssl/aes.h>
-
-void handle_client(int client_socket) {
-    // Send the key immediately upon connection
-    const char *key = "S send_flag.txt\n";
-    send(client_socket, key, strlen(key), 0);
-
-    char command[2];
-    while (1) {
-        // Receive and handle commands from the client
-        if (recv(client_socket, command, sizeof(command), 0) <= 0) {
-            // Error or client closed the connection
-            break;
-        }
-
-        switch (command[0]) {
-            case 'S':
-                // Handle the "S" command
-                // ...
-                break;
-            case 'F':
-                // Handle the "F" command (if needed)
-                // ...
-                break;
-            // Handle other commands as needed
-        }
-    }
-
-    // Close the client socket when done
-    close(client_socket);
-}
 
 int main() {
-    int server_socket;
-    struct sockaddr_in server_addr;
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  int option = 1;
+  setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 
-    // Create socket
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        perror("Error creating socket");
-        exit(EXIT_FAILURE);
-    }
+  if (server_socket == -1) {
+    perror("Error creating socket");
+    exit(EXIT_FAILURE);
+  }
 
-    // Configure server address
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(1234);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+  struct sockaddr_in server_addr;
 
-    // Bind socket to address
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Error binding socket");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(1234);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    // Listen for incoming connections
-    if (listen(server_socket, 5) == -1) {
-        perror("Error listening for connections");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server listening on port 1234...\n");
-
-    while (1) {
-        struct sockaddr_in client_addr;
-        int client_socket;
-        socklen_t client_addr_len = sizeof(client_addr);
-
-        // Accept incoming connection
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
-        if (client_socket == -1) {
-            perror("Error accepting client connection");
-            continue;
-        }
-
-        // Handle the client
-        handle_client(client_socket);
-    }
-
-    // Close the server socket (never reached in this example)
+  if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+    perror("Error binding socket");
     close(server_socket);
+    exit(EXIT_FAILURE);
+  }
 
-    return 0;
+  // Listen for incoming connections
+  if (listen(server_socket, 5) == -1) {
+    perror("Error listening for connections");
+    close(server_socket);
+    exit(EXIT_FAILURE);
+  }
+
+  // Accept incoming connections
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+  int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
+  if (client_socket == -1) {
+    perror("Error accepting connection");
+    close(server_socket);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Client connected\n");
+
+  // Main server logic goes here
+
+  char buf[128];
+  // S
+  // Fflag.txt
+  // PPrestidigitation
+  // S
+  char *line;
+
+  line = "Fflag.txt\n";
+  send(client_socket, line, 128, 0);
+  printf("Sent: %s", line);
+
+  line = "PPrestidigitation\n";
+  send(client_socket, line, 128, 0);
+  printf("Sent: %s", line);
+
+  line = "S\n";
+  send(client_socket, line, 128, 0);
+  printf("Sent: %s", line);
+
+  while(recv(client_socket, buf, 128, 0))
+    printf("Received: %s\n", buf);
+
+  close(client_socket);
+  close(server_socket);
+
+  return 0;
 }
 
